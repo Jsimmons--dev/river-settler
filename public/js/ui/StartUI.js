ef468b8cbbbbb7a82cacf76fb3a1f4b781cc13d5
Josh Simmons <mmn.jsimmons@gmail.com>
2017-05-20 17:18:14 -0400

    updated main menu button layout

--- public/js/ui/StartUI.js
+++ public/js/ui/StartUI.js
@@ -8,17 +8,19 @@ export class StartUI extends BaseUI{
         this.controller = controller;
        
         let optionsEle = document.createElement('a');
-        optionsEle.className = "col offset-s3 s6 btn waves-effect waves-light";
+        optionsEle.className = "col offset-s2 s8 btn waves-effect waves-light";
+        optionsEle.style.margin = '2% 1% 1% 1%';
+        optionsEle.style.width = '98%';
         optionsEle.appendChild(document.createTextNode("Options"));
         optionsEle.addEventListener('click',this.controller.fadeToOptions);
         uiRoot.style.background = '#88ddff'; 
         uiRoot.innerHTML = `
             <div id='startCanvas' style='height:0%;'></div>
             <div class="col" style="height:50%"></div>
-            <div id='startButtons' class="row">
-                <a id='startGameButton' class="col offset-s3 s6 btn waves-effect waves-light">Start New Game</a>
-                <a class="col offset-s3 s6 btn waves-effect waves-light">Load Scenario</a>
-                <a class="col offset-s3 s6 btn waves-effect waves-light">Scenario Editor</a>
+            <div id='startButtons' class="row card light-blue lighten-4" style='width:50%; margin:auto'>
+                <a id='startGameButton' class="col btn waves-effect waves-light" style='width:98%;margin: 1% 1% 0 1%'>Start New Game</a>
+                <a class="col offset-s2 s8 btn waves-effect waves-light" style='width:98%;margin: 2% 1% 0 1%'>Load Scenario</a>
+                <a class="col offset-s2 s8 btn waves-effect waves-light" style='width:98%;margin: 2% 1% 0 1%'>Scenario Editor</a>
             </div>
             `;
 
